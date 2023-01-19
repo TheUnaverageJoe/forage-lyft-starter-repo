3ac38547266f58c34eed8249dbe89a248ad3f5ac
TheUnaverageJoe <jcarter94043@gmail.com>
2023-01-19 12:37:02 -0800

    added tires to car factory methods
    Assigned placeholder tires to avoid errors

--- Car.py
+++ Car.py
@@ -1,14 +1,16 @@
 from IServicable import IServicable
 from Batteries.IBattery import IBattery
 from Engines.IEngine import IEngine
+from Tires.ITire import ITire
 
 class Car(IServicable):
-    def __init__(self, engine: IEngine, battery: IBattery) -> None:
+    def __init__(self, engine: IEngine, battery: IBattery, tires: ITire) -> None:
         self.engine = engine
         self.battery = battery
+        self.tires = tires
 
     def needs_service(self) -> bool:
-        if self.engine.needs_service() or self.battery.needs_service():
+        if self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service():
             return True
         else:
             return False
--- CarFactory.py
+++ CarFactory.py
@@ -4,16 +4,22 @@ from Engines.CapuletEngine import CapuletEngine
 from Engines.WilloughbyEngine import WilloughbyEngine
 from Batteries.SpindlerBattery import SpindlerBattery
 from Batteries.NubbinBattery import NubbinBattery
+from Tires.OctoprimeTire import OctoprimeTire
+from Tires.CarriganTire import CarriganTire
 from Car import Car
 
 class CarFactory():
-    def create_calliope(self, current_date: datetime, last_service_date: datetime, current_milage: int, last_service_milage: int) -> Car:
-        return Car(CapuletEngine(last_service_milage, current_milage), SpindlerBattery(last_service_date, current_date))
-    def create_glissade(self, current_date: datetime, last_service_date: datetime, current_milage: int, last_service_milage: int) -> Car:
-        return Car(WilloughbyEngine(last_service_milage, current_milage), SpindlerBattery(last_service_date, current_date))
-    def create_palindrome(self, current_date: datetime, last_service_date: datetime, warning_light_on: bool) -> Car:
-        return Car(SternmanEngine(warning_light_on), SpindlerBattery(last_service_date, current_date))
-    def create_rorschach(self, current_date: datetime, last_service_date: datetime, current_milage: int, last_service_milage: int) -> Car:
-        return Car(WilloughbyEngine(last_service_milage, current_milage), NubbinBattery(last_service_date, current_date))
-    def create_thovex(self, current_date: datetime, last_service_date: datetime, current_milage: int, last_service_milage: int) -> Car:
-        return Car(CapuletEngine(last_service_milage, current_milage), NubbinBattery(last_service_date, current_date))
+    def create_calliope(self, current_date: datetime, last_service_date: datetime, current_milage: int, last_service_milage: int, tire_wear: list[4]) -> Car:
+        return Car(CapuletEngine(last_service_milage, current_milage), SpindlerBattery(last_service_date, current_date), CarriganTire(tire_wear))
+
+    def create_glissade(self, current_date: datetime, last_service_date: datetime, current_milage: int, last_service_milage: int, tire_wear: list[4]) -> Car:
+        return Car(WilloughbyEngine(last_service_milage, current_milage), SpindlerBattery(last_service_date, current_date), CarriganTire(tire_wear))
+
+    def create_palindrome(self, current_date: datetime, last_service_date: datetime, warning_light_on: bool, tire_wear: list[4]) -> Car:
+        return Car(SternmanEngine(warning_light_on), SpindlerBattery(last_service_date, current_date), CarriganTire(tire_wear))
+
+    def create_rorschach(self, current_date: datetime, last_service_date: datetime, current_milage: int, last_service_milage: int, tire_wear: list[4]) -> Car:
+        return Car(WilloughbyEngine(last_service_milage, current_milage), NubbinBattery(last_service_date, current_date), OctoprimeTire(tire_wear))
+
+    def create_thovex(self, current_date: datetime, last_service_date: datetime, current_milage: int, last_service_milage: int, tire_wear: list[4]) -> Car:
+        return Car(CapuletEngine(last_service_milage, current_milage), NubbinBattery(last_service_date, current_date), OctoprimeTire(tire_wear))
